698a96a4ace0a163c1713e4c0c7cd511e924276e
Reason <77286260+reasonlive@users.noreply.github.com>
2025-07-25 21:28:24 +0300

    del

--- nest/src/config.ts
+++ nest/src/config.ts
@@ -3,6 +3,6 @@ export default {
     ENV: 'dev', // dev/prod,
     AUTH: true,
     API_URL_DEV: 'http://localhost:3030',
-    API_URL_PROD: 'https://cryptoservice.bingopay.io',
+    API_URL_PROD: '',
     JWT_SECRET: 'd2d76b87336220013bcc5fa8c81c26b30baa4f4ff74134f5215126d31249829c',
 }
